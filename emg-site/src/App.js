6a83b62b9dd59c4feb2940be10014cb8cccb9e06
damiles1 <damiles1@asu.edu>
2026-05-01 22:11:28 -0700

    Update App.js

--- emg-site/src/App.js
+++ emg-site/src/App.js
@@ -165,9 +165,9 @@ function App() {
               Female & Male Results.
             </p>
 
-            <ImageBox title="SNR vs Electrode Count" src="/SNRfemale.png" />
-            <ImageBox title="Normalized RMS" src="/NormalizedRMSFemale.png" />
-            <ImageBox title="Rest-to-Contraction Difference" src="/ResttoContractionFemale.png" />
+            <ImageBox title="SNR vs Electrode Count" src="/SNRfemale.png" src="/SNRmale.png"/>
+            <ImageBox title="Normalized RMS" src="/NormalizedRMSFemale.png" src="/MaleRMS.png/">
+            <ImageBox title="Rest-to-Contraction Difference" src="/ResttoContractionFemale.png" src="/RestFullMale.png/"/>
           </section>
         )}
 
