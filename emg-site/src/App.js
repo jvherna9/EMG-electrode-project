50047f2400a547c4ab41fea227a4aba5758efee4
damiles1 <damiles1@asu.edu>
2026-05-01 20:19:35 -0700

    App.js

--- emg-site/src/App.js
+++ emg-site/src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <div style={styles.page}>
       <header style={styles.header}>
-        <h1 style={{ ...styles.title, marginBottom: "25px" }}>Optimizing EMG Signals for Better Prosthetic Performance ᕙ(⇀‸↼‶)ᕗ</h1>
+        <h1 style={{ ...styles.title, marginBottom: "30px" }}>Optimizing EMG Signals for Better Prosthetic Performance ᕙ(⇀‸↼‶)ᕗ</h1>
     
         <p style={styles.subtitle}>
           Comparing 2, 4, and 6 electrode configurations for EMG signal detection!
